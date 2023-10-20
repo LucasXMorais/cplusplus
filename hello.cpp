6be35541aca4a19ca9bf59a9200b65453123396e
Lucas Morais <lucas.xavier.de.morais@gmail.com>
2023-10-20 18:12:19 -0300

    Strating cpp course

---
+++ hello.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+int main(){
+
+  std::string x = "abc";
+  /* const char* x = "abc"; */
+  /* bool x = true; */
+  std::cout << x << std::endl;
+  std::cout << sizeof(x) << std::endl;
+
+  return 0;
+
+}
